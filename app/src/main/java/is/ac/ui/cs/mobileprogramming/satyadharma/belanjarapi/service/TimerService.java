c0b62a3ef36ada9b0e650cc9f86521e8c4616cf7
SatyaDh <satya1chiko@gmail.com>
2020-12-30 13:05:06 +0700

    feat: add notification to service

--- app/src/main/java/is/ac/ui/cs/mobileprogramming/satyadharma/belanjarapi/service/TimerService.java
+++ app/src/main/java/is/ac/ui/cs/mobileprogramming/satyadharma/belanjarapi/service/TimerService.java
@@ -1,25 +1,51 @@
 package is.ac.ui.cs.mobileprogramming.satyadharma.belanjarapi.service;
 
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.CountDownTimer;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+import is.ac.ui.cs.mobileprogramming.satyadharma.belanjarapi.R;
+import is.ac.ui.cs.mobileprogramming.satyadharma.belanjarapi.activity.MainActivity;
 
 public class TimerService extends Service {
     private final static String TAG = "TimerService";
-
+    public static final String CHANNEL_ID = "is.ac.ui.cs.mobileprogramming.satyadharma.belanjarapi.notifId";
     public static final String COUNTDOWN_BR = "is.ac.ui.cs.mobileprogramming.satyadharma.belanjarapi.countdown_br";
     Intent bi = new Intent(COUNTDOWN_BR);
+
+    private NotificationManager notificationManager;
+    private Notification noti;
+
 
     CountDownTimer cdt = null;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        notificationManager =
+                (NotificationManager) getSystemService(Service.NOTIFICATION_SERVICE);
+        createNotificationChannel(notificationManager);
+        Intent notificationIntent = new Intent(getApplicationContext(), MainActivity.class);
+        PendingIntent pending = PendingIntent.getActivity(this,0,notificationIntent,0);
+        noti = new NotificationCompat.Builder(getApplicationContext(),CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText("Done!")
+                .setContentInfo("Your Shopping should be done by now!")
+                .setContentIntent(pending)
+                .setAutoCancel(true)
+                .build();
+
         cdt = new CountDownTimer(30000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
@@ -33,17 +59,20 @@ public class TimerService extends Service {
             public void onFinish() {
 
                 Log.i(TAG, "Timer finished");
+                notificationManager.notify(0, noti);
+                onDestroy();
             }
         };
 
         cdt.start();
+
+
     }
 
     @Override
     public void onDestroy() {
 
         cdt.cancel();
-        Log.i(TAG, "Timer cancelled");
         super.onDestroy();
     }
 
@@ -58,4 +87,20 @@ public class TimerService extends Service {
     public IBinder onBind(Intent intent) {
         return null;
     }
+
+    private void createNotificationChannel(NotificationManager notificationManager) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.notification);
+            String description = getString(R.string.notification_desc);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 }
